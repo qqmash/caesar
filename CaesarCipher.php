af054ff4f0e7fa4a9d25b8405694612482bb4536
BuildTools <unconfigured@null.spigotmc.org>
2018-02-03 23:38:05 +0600

    indentation fix

--- CaesarCipher.php
+++ CaesarCipher.php
@@ -13,30 +13,31 @@ class CesarCipher {
         $this->shift = $shift;
     }
 
-    public function encode($plainText){
+    public function encode($plainText)
+    {
         // Реализация
-		$alphabet = $this->alphabet;
-		$shift = $this->shift;
-		$encoded_text = '';
-		$code = $alphabet;
-		
-		for ($i = 0; $i < $shift; $i++) {
-			$char = mb_substr($code, 0, 1, 'utf-8');
-			$code = mb_substr($code, 1, mb_strlen($code, 'utf-8'), 'utf-8') . $char;
-		}
-		
-		$textLength = mb_strlen($plainText, 'utf-8');
+        $alphabet = $this->alphabet;
+        $shift = $this->shift;
+        $encoded_text = '';
+        $code = $alphabet;
+        
+        for ($i = 0; $i < $shift; $i++) {
+            $char = mb_substr($code, 0, 1, 'utf-8');
+            $code = mb_substr($code, 1, mb_strlen($code, 'utf-8'), 'utf-8') . $char;
+        }
+        
+        $textLength = mb_strlen($plainText, 'utf-8');
 
-		for ($i = 0; $i < $textLength; $i++) {
-			$char = mb_substr($plainText, $i, 1, 'utf-8');
-			$pos = mb_strpos($alphabet, $char, 0, 'utf-8');
+        for ($i = 0; $i < $textLength; $i++) {
+            $char = mb_substr($plainText, $i, 1, 'utf-8');
+            $pos = mb_strpos($alphabet, $char, 0, 'utf-8');
 
-			if (false !== $pos) {
-				$encodedText .= mb_substr($code, $pos, 1, 'utf-8');
-			} else {
-				$encodedText .= $char;
-			}
-		}
+            if (false !== $pos) {
+                $encodedText .= mb_substr($code, $pos, 1, 'utf-8');
+            } else {
+                $encodedText .= $char;
+            }
+        }
         return $encodedText;
     }
 }
