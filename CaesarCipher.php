7b6d486b173e77e2bd136a673a6b30b9e74989aa
BuildTools <qqmash@mail.ru>
2018-03-22 11:37:58 +0600

    get rid of unnecessary variables

--- CaesarCipher.php
+++ CaesarCipher.php
@@ -15,13 +15,10 @@ class CaesarCipher {
 
     public function encode($plainText)
     {
-        // Реализация
-        $alphabet = $this->alphabet;
-        $shift = $this->shift;
         $encoded_text = '';
-        $code = $alphabet;
+        $code = $this->alphabet;
         
-        for ($i = 0; $i < $shift; $i++) {
+        for ($i = 0; $i < $this->shift; $i++) {
             $char = mb_substr($code, 0, 1, 'utf-8');
             $code = mb_substr($code, 1, mb_strlen($code, 'utf-8'), 'utf-8') . $char;
         }
@@ -30,7 +27,7 @@ class CaesarCipher {
 
         for ($i = 0; $i < $textLength; $i++) {
             $char = mb_substr($plainText, $i, 1, 'utf-8');
-            $pos = mb_strpos($alphabet, $char, 0, 'utf-8');
+            $pos = mb_strpos($this->alphabet, $char, 0, 'utf-8');
 
             if (false !== $pos) {
                 $encodedText .= mb_substr($code, $pos, 1, 'utf-8');
@@ -38,6 +35,7 @@ class CaesarCipher {
                 $encodedText .= $char;
             }
         }
+
         return $encodedText;
     }
 }
